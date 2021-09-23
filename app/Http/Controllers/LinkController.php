2a352e0cca054fc1c898daee43480176e313329e
Michael Barrows <mail@michaelbarrows.com>
2021-09-23 21:18:46 +0100

    a small start

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Link;
+
+class LinkController extends Controller
+{
+    public function redirectToDestination($shortLink)
+    {
+        $shortLink = Link::query()->whereShortLink($shortLink)->first();
+
+        if (! $shortLink) {
+            abort(404);
+        }
+
+        return redirect()->away($shortLink->destination_link);
+    }
+}
