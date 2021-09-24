25723209c0ee6abda92ca009f034d867c0fa0c0e
Michael Barrows <mail@michaelbarrows.com>
2021-09-24 18:13:25 +0100

    comment out auth routes temporarily

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 Route::domain('i.' . env('ROOT_DOMAIN'))->group(function () {
     Route::get('/{shortLink}', [LinkController::class, 'redirectToDestination']);
 });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
